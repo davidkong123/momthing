263333913c9b661ab5da07c7d78d38cdc9b8a0d8
mr.dogo <davidbekker2005@gmail.com>
2023-05-31 22:42:07 +0300

    ver5

--- app/src/main/java/com/example/mom/MainActivity.java
+++ app/src/main/java/com/example/mom/MainActivity.java
@@ -17,10 +17,18 @@ public class MainActivity extends AppCompatActivity {
     Button[] btns = new Button[3];
     RecyclerView[] recyclerViews = new RecyclerView[3];
 
+    Boolean flag;
+    Boolean flag2;
+    Boolean flag3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        flag = false;
+        flag2 = false;
+        flag3 = false;
 
         btns[0] = findViewById(R.id.button);
         btns[1] = findViewById(R.id.button2);
@@ -33,7 +41,31 @@ public class MainActivity extends AppCompatActivity {
         String[] lans = {"Language 1", "Language 2", "Language 3"};
 
         List<Item> items1 = new ArrayList<>();
-        items1.add(new Item("מה נשמעה 1", lans));
+        items1.add(new Item("האם כואב לך?", lans));
+        items1.add(new Item("האם אתה רוצה להתקלח?", lans));
+        items1.add(new Item("האם אתה רוצה תרופות נגד כאבים?", lans));
+        items1.add(new Item("איזה תרופות אתה לוקח?", lans));
+        items1.add(new Item("האם אתה צריך עזרה?", lans));
+        items1.add(new Item("האם יש לך רגישות לתרופות?", lans));
+        items1.add(new Item("חום", lans));
+        items1.add(new Item("לחץ דם", lans));
+        items1.add(new Item("דופק", lans));
+        items1.add(new Item("כן", lans));
+        items1.add(new Item("לא", lans));
+        items1.add(new Item("שירותים", lans));
+        items1.add(new Item("נשימה", lans));
+        items1.add(new Item("בדיקת דם", lans));
+        items1.add(new Item("כמה זמן", lans));
+        items1.add(new Item("שתן", lans));
+        items1.add(new Item("יציאה", lans));
+        items1.add(new Item("שלום", lans));
+        items1.add(new Item("תודה", lans));
+        items1.add(new Item("בבקשה", lans));
+        items1.add(new Item("מה שלומך?", lans));
+        items1.add(new Item("האם אכלת?", lans));
+        items1.add(new Item("סוכר", lans));
+        items1.add(new Item("מה השם שלך?", lans));
+
 
         List<Item> items2 = new ArrayList<>();
         items2.add(new Item("מה נשמעה 2", lans));
@@ -60,15 +92,34 @@ public class MainActivity extends AppCompatActivity {
         recyclerViews[2].setVisibility(View.GONE); // Hide RecyclerView 3
 
         if (view == btns[0]) {
-            recyclerViews[0].setVisibility(View.VISIBLE);
-            Toast.makeText(this, "1", Toast.LENGTH_SHORT).show();
+            if (!flag) {
+                recyclerViews[0].setVisibility(View.VISIBLE);
+                flag = true;
+            }
+            else{
+                recyclerViews[0].setVisibility(View.GONE);
+                flag = false;
+            }
+
         } else if (view == btns[1]) {
-            recyclerViews[1].setVisibility(View.VISIBLE);
-            Toast.makeText(this, "2", Toast.LENGTH_SHORT).show();
+            if (!flag2) {
+                recyclerViews[1].setVisibility(View.VISIBLE);
+                flag2 = true;
+            }
+            else{
+                recyclerViews[1].setVisibility(View.GONE);
+                flag2 = false;
+            }
         }
         else if (view == btns[2]) {
-            recyclerViews[2].setVisibility(View.VISIBLE);
-            Toast.makeText(this, "3", Toast.LENGTH_SHORT).show();
+            if (!flag3) {
+                recyclerViews[2].setVisibility(View.VISIBLE);
+                flag3 = true;
+            }
+            else{
+                recyclerViews[2].setVisibility(View.GONE);
+                flag3 = false;
+            }
         }
     }
 }
